1885830fbf1baaa0a3ca7cda56156c59d36fd7c8
JakobEsp <jakobespersen0@gmail.com>
2023-01-18 14:14:08 +0100

    last fixesw

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 namespace h1Converter
 {
     class Program
@@ -7,33 +6,50 @@ namespace h1Converter
         
         static void Main(string[] args)
         {
-            string key;
+            ConsoleKeyInfo key;
             Console.WriteLine("What Would you like to convert?");
             Console.WriteLine("cm -> inches[D]");
+            Console.WriteLine("Celsius -> Farenheit[F]");
             do
             {
-                key = Console.ReadKey().ToString();
+                key = Console.ReadKey();
             } while (key == null);
-            float result = GetConvertion();
+            string result = GetConvertion(key.Key.ToString());
+            Console.Clear();
+            Console.WriteLine($"you result was {result}");
+            Console.ReadLine();
         }
 
-        public float GetConvertion(string key)
+        private static string GetConvertion(string key)
         {
             float result;
+            string resolution;
             float number;
             
-            switch(key.ToString()){
+            switch(key.ToUpper()){
                 case "D":
+                    Console.Clear();
                     Console.WriteLine("How many cm?");
                     number = Convert.ToInt32(Console.ReadLine());
+                    
                     result = number * 0.393700787f;
+                    resolution = "Inches";
+                    break;
+                case "F":
+                    Console.Clear();
+                    Console.WriteLine("How much Celsuis?");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    
+                    result = (number * 1.8f) + 32;
+                    resolution = "Farhenheit";
                     break;
                 default:
-                    Console.WriteLine("fok of");
                         result = 0f;
+                        resolution = "fok of";
                     break;
             }
-            return result;
+            Console.WriteLine(key);
+            return $"{result} {resolution}";
         }
     }
 }
